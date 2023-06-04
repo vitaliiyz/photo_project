213d3963a53bc969f235d8a4f54afd2799348eb5
vitaliiyz <vz260198@gmail.com>
2023-06-04 17:43:31 +0200

    added and connected static

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
 from home.views import home_page
 
-urlpatterns = [
-    path("", home_page, name="home_page")
-]
+urlpatterns = [path("", home_page, name="home_page")]
